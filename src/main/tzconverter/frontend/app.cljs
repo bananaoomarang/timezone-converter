20d3709c4e29448f372d9b4e8bb8e811179f58eb
Milo Mordaunt <milomord@gmail.com>
2024-03-25 15:42:41 -0400

    Add functionality/basic UI

--- src/main/tzconverter/frontend/app.cljs
+++ src/main/tzconverter/frontend/app.cljs
@@ -2,10 +2,60 @@
   (:require
    [goog.dom :as gdom]
    [reagent.core :as r]
-   ["react-dom/client" :refer [createRoot]]))
+   ["react-dom/client" :refer [createRoot]]
+   ["date-fns-tz" :refer [zonedTimeToUtc, utcToZonedTime, format]]))
+
+;; Use now for defaults
+(def now-date (.now js/Date))
+
+(def timezones (.supportedValuesOf js/Intl "timeZone"))
+;; (def timezones ["America/New_York" "America/Los_Angeles"])
+(def user-timezone (-> js/Intl
+                       .DateTimeFormat
+                       .resolvedOptions
+                       .-timeZone))
+
+(defonce selected-date (r/atom (format now-date "yyyy-MM-dd")))
+(defonce selected-time (r/atom (format now-date "HH:mm")))
+(defonce selected-from-timezone (r/atom user-timezone))
+(defonce selected-to-timezone (r/atom "America/Los_Angeles"))
+
+(defn translate-timezone [datetime from-zone to-zone]
+  (let [utc-date (zonedTimeToUtc (js/Date. datetime) from-zone)
+        zoned-date (utcToZonedTime utc-date to-zone)]
+    (format zoned-date "Mo 'of' LLLL yyyy 'at' h:mm b" (clj->js {:timeZone to-zone}))))
+
+(defn select-timezone [label value]
+  [:label label
+   [:select {:value @value
+             :on-change #(reset! value (-> % .-target .-value))}
+    (for [timezone timezones]
+      ^{:key timezone}
+      [:option {:value timezone} timezone])]])
+
+(defn select-time [label date-value time-value]
+  [:label label
+   [:input {:type "date"
+            :value @date-value
+            :on-change #(reset! date-value (-> % .-target .-value))}]
+   [:input {:type "time"
+            :value @time-value
+            :on-change #(reset! time-value (-> % .-target .-value))}]])
+
+(defn result []
+  [:h3 "Result time is: "
+   (translate-timezone
+    (str @selected-date " " @selected-time)
+    @selected-from-timezone
+    @selected-to-timezone)])
 
 (defn app []
-  [:h1 "hello"])
+  [:div
+   [:div
+    [select-time "Input Time" selected-date selected-time]
+    [select-timezone "Input Timezone" selected-from-timezone]
+    [select-timezone "Output Timezone" selected-to-timezone]]
+   [result]])
 
 (defonce root (createRoot (gdom/getElement "root")))
 
